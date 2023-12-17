7bf6cbef643b6b1647fe3f4293b437184a93ba8a
OmnidroidOfficial <144283173+OmnidroidOfficial@users.noreply.github.com>
2023-12-17 22:17:31 +0100

    Create Participants.cs

---
+++ Scripts/Participants.cs
@@ -0,0 +1,26 @@
+> Participants.cs 0.0 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Participants : MonoBehaviour
+{
+    static public int ParticipantsCount;
+
+    // Start is called before the first frame update
+
+    static public void OnlineParticipants() 
+    { 
+        // if HasSpectators == true => if Aye = false => spectator may overtake player number
+    }
+
+    static public void OfflineParticipants()
+    {
+
+    }
+
+    // Update is called once per frame
+    
+}
