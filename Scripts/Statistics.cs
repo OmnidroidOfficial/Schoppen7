303c46f3c4cf3209562d6b63d0edbce0296ec4bd
OmnidroidOfficial <144283173+OmnidroidOfficial@users.noreply.github.com>
2023-12-17 22:14:14 +0100

    Create Statistics.cs

---
+++ Scripts/Statistics.cs
@@ -0,0 +1,32 @@
+> Statistics.cs 0.0
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Statistics : MonoBehaviour
+{
+    static public int FavoriteAvatar;
+    static public int SelectAvatar1Count;
+    static public int SelectAvatar2Count;
+    static public int SelectAvatar3Count;
+    static public int SelectAvatar4Count;
+    static public int SelectAvatar5Count;
+    static public int SelectAvatar6Count;
+    static public int SelectAvatar7Count;
+    static public int SelectAvatar8Count;
+    static public int SelectAvatar9Count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
