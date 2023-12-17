faa83ccb2024da13c771e9f0cf059fad9cdae4a5
OmnidroidOfficial <144283173+OmnidroidOfficial@users.noreply.github.com>
2023-12-17 22:16:11 +0100

    Create GameInfo.cs

---
+++ Scripts/GameInfo.cs
@@ -0,0 +1,61 @@
+> GameInfo.cs 0.0
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameInfo : MonoBehaviour
+{
+    static public Sprite AvatarImage1;
+    static public Sprite AvatarImage2;
+    static public Sprite AvatarImage3;
+    static public Sprite AvatarImage4;
+    static public Sprite AvatarImage5;
+    static public Sprite AvatarImage6;
+    static public Sprite AvatarImage7;
+    static public Sprite AvatarImage8;
+    static public Sprite AvatarImage9;
+    static public int MaxSpectators;
+
+    public void AvatarOne()
+    {
+
+    }
+
+    public void AvatarTwo()
+    {
+
+    }
+
+    public void AvatarThree()
+    {
+
+    }
+    public void AvatarFour()
+    {
+
+    }
+    public void AvatarFive()
+    {
+
+    }
+    public void AvatarSix()
+    {
+
+    }
+    public void AvatarSeven()
+    {
+
+    }
+    public void AvatarEight()
+    {
+
+    }
+    public void AvatarNine()
+    {
+
+    }
+}
